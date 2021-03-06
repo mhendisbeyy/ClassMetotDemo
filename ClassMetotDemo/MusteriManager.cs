882806ec9db8c9ae6200779369ab3debee226e8b
Ali ATALAY <mhendisbey@gmail.com>
2021-03-07 01:45:00 +0300

    Müşteri Takibi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void MusteriEkle(Musteri[] musteriler)
+        {
+            Musteri musteri4 = new Musteri();
+            musteri4.Ad = "Ünsal";
+            musteri4.Soyad = "KARASU";
+            musteri4.Yas = 28;
+            musteri4.Aciklama = "Mühendis";
+            musteriler[2] = musteri4;
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                if(musteri!=null)
+                {
+                    Console.WriteLine("Müsteri Ad Soyad : " + musteri.Ad + " " + musteri.Soyad);
+                    Console.WriteLine("Yaş : " + musteri.Yas);
+                    Console.WriteLine("Açıklama " + musteri.Aciklama);
+                    Console.WriteLine("-----------------------------");
+                }
+            }
+        }
+        public void MusteriSil(Musteri[] musteriler)
+        {
+            Array.Clear(musteriler, 1, 2);
+            Console.WriteLine("Müşteriler Silindi.");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,56 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
+            try
+            {
+                MusteriManager musteriManager = new MusteriManager();
+                Musteri musteri1 = new Musteri();
+                musteri1.Ad = "Ali";
+                musteri1.Soyad = "ATALAY";
+                musteri1.Yas = 27;
+                musteri1.Aciklama = "Öğrenci";
+
+                Musteri musteri2 = new Musteri();
+                musteri2.Ad = "Burcu";
+                musteri2.Soyad = "AKEL";
+                musteri2.Yas = 30;
+                musteri2.Aciklama = "Okul Öncesi Öğretmeni";
+
+                Musteri musteri3 = new Musteri();
+                musteri3.Ad = "Hüseyin";
+                musteri3.Soyad = "ATALAY";
+                musteri3.Yas = 33;
+                musteri3.Aciklama = "Sosyal Bilgiler Öğretmeni";
+
+                Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+
+                //Müşteri Listeleme
+
+                musteriManager.MusteriListele(musteriler);
+                //Müşteri Listeleme
+                // Müşteri Ekle
+                musteriManager.MusteriEkle(musteriler);
+                Console.WriteLine("Müşteri Eklendi");
+                musteriManager.MusteriListele(musteriler);
+                //Müşteri Ekle
+
+                //Müşteri Silme
+                musteriManager.MusteriSil(musteriler);
+                //Müşteri Silme
+
+                //Dizideki müşteri silindikten sonra Müşteri Listeleme
+                Console.WriteLine("Dizideki müşteri silindikten sonra Müşteri Listeleme");
+                musteriManager.MusteriListele(musteriler);
+                //Dizideki müşteri silindikten sonra Müşteri Listeleme
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
